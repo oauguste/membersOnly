637ef799534d33a7e25a4541fcf242cf497fa3ec
oauguste <augusteosnac@gmail.com>
2023-08-30 19:15:25 -0400

    added correct db path

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const MongoStore = require("connect-mongo");
 
 const app = express();
 const dev_db_url =
-  "mongodb+srv://your_user_name:your_password@cluster0.lz91hw2.mongodb.net/local_library?retryWrites=true&w=majority";
+  "mongodb+srv://augusteosnac:helixx0099@cluster0.k07zqhb.mongodb.net/?retryWrites=true&w=majority";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.set("strictQuery", false);
 //views engine setup
